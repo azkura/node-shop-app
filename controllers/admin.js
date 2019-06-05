1a2ec2fb70c6541a037cded0900d33cf787c3e3a
az <azizkoura@hotmail.com>
2019-06-05 15:18:11 +0200

    checking if product null create product else edit

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,8 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  //if null (id) we can create a new product
+  const product = new Product(null,title, imageUrl, description, price);
   product.save();
   res.redirect('/')
 }
