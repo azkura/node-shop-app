12021632cdee53615de9bd8e17ae04ff26d96f73
azkura <azizkoura@hotmail.com>
2019-06-08 04:05:54 +0200

    adding sequelize to connect to databbse

--- helpers/database.js
+++ helpers/database.js
@@ -1,10 +1,19 @@
-const mysql = require('mysql2')
+// const mysql = require('mysql2')
 
-const pool = mysql.createPool({
-    host:'localhost',
-    user: 'root',
-    database: 'node-shop',
-    password: 'pandemon'
+// const pool = mysql.createPool({
+//     host:'localhost',
+//     user: 'root',
+//     database: 'node-shop',
+//     password: 'pandemon'
+// })
+
+// module.exports = pool.promise()
+
+const Sequelise = require('sequelize')
+
+const sequelize = new Sequelise('node-shop', 'root', 'pandemon', {
+    dialect: 'mysql',
+    host: 'localhost'
 })
 
-module.exports = pool.promise()
+module.exports = sequelize
