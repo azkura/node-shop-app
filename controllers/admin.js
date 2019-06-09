a85e917e15e2c6df76ce91e6d66c965913802088
azkura <azizkoura@hotmail.com>
2019-06-09 14:26:17 +0200

    updating product

--- controllers/admin.js
+++ controllers/admin.js
@@ -25,38 +25,49 @@ exports.postAddProduct = (req, res, next) => {
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
   if (!editMode) {
-    return res.redirect('/');
+    return res.redirect('/'); 
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    if (!product) {
-      return res.redirect('/');
+  Product.findByPk(prodId).then(product => {
+    if(!product) {
+      return res.redirect('/')
     }
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
+      path: 'admin/edit-product',
       editing: editMode,
       product: product
-    });
-  });
-};
+    })
+  }).catch(err => console.log(err))
+}
 
 exports.postEditProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  const updatedTitle = req.body.title;
-  const updatedPrice = req.body.price;
-  const updatedImageUrl = req.body.imageUrl;
-  const updatedDesc = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDesc,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect('/admin/products');
-};
+  const prodId = req.body.productId
+  const updatedTitle = req.body.title
+  const updatedPrice = req.body.price
+  const updatedImageUrl = req.body.imageUrl
+  const updatedDesc = req.body.description
+  Product.findByPk(prodId).then(product => {
+    product.title = updatedTitle,
+    product.price = updatedPrice,
+    product.imageUrl = updatedImageUrl,
+    product.description = updatedDesc
+    //return promess to save into db
+    return product.save()
+  }).then(result => {
+    console.log('UPDATED PRODUCT!')
+    res.redirect('/admin/products')
+  })
+  .catch(err => console.log(err))
+  // const updatedProduct = new Product(
+  //   prodId,
+  //   updatedTitle,
+  //   updatedImageUrl,
+  //   updatedDesc,
+  //   updatedPrice
+  // );
+  // updatedProduct.save()  
+}
 
 exports.getProducts = (req, res, next) => {
   Product.findAll().then(products => {
