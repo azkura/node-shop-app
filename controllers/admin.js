5495f53c3e1ec8654d0edbca8943f3d7a9591dcd
azkura <azizkoura@hotmail.com>
2019-06-09 14:40:04 +0200

    deleting product

--- controllers/admin.js
+++ controllers/admin.js
@@ -18,7 +18,11 @@ exports.postAddProduct = (req, res, next) => {
     price: price,
     imageUrl: imageUrl,
     description: description
-  }).then(res => console.log(res))
+  })
+  .then(res => {
+    console.log('CREATED PRODUCT!')
+    res.redirect('/admin/products')
+  })
   .catch(err => console.log(err))
 }
 
@@ -80,7 +84,15 @@ exports.getProducts = (req, res, next) => {
 }
 
 exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
-};
+  const prodId = req.body.productId
+  Product.findByPk(prodId).then(product => {
+    return product.destroy()
+  }).then(result => {
+    console.log('DESTROYED PRODUCT!')
+    res.redirect('/admin/products')
+  })
+  .catch(err => console.log(err))
+  
+  // Product.deleteById(prodId)
+  // res.redirect('/admin/products')
+}
