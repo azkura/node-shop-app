24fcaab174f9bc5862167ae13e469b14ba2d949e
azkura <azizkoura@hotmail.com>
2019-06-09 23:04:10 +0200

    adding existing product into cart & retreiving too

--- controllers/shop.js
+++ controllers/shop.js
@@ -71,6 +71,7 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId
   let fetchedCart
+  let newQuantity = 1
   req.user.getCart()
   .then(cart => {
     fetchedCart = cart
@@ -81,18 +82,18 @@ exports.postCart = (req, res, next) => {
     if(products.length > 0) {
       product = products[0]
     }
-    let newQuantity = 1
     if(product) {
-      //..
+      const oldQuantity = product.cartItem.quantity
+      newQuantity = oldQuantity + 1
+      return product
     }
     //add a product first time
-    Product.findByPk(prodId)
-    .then(product => {
-      return fetchedCart.addProduct(product, { 
-        through: { quantity: newQuantity } 
-      })
-    })
-    .catch(err => console.log(err))
+     return Product.findByPk(prodId)
+  })
+  .then(product => {
+    return fetchedCart.addProduct(product, {
+      through: { quantity: newQuantity }
+    })  
   })
   .then(() => {
     res.redirect('/')
